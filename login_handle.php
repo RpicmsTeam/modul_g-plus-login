8f0266352b6447cd2b7a35e9cfa8c4dda9682069
MTRNord <mtrnord1@gmail.com>
2014-12-30 21:59:15 +0100

    the ap is confusing :D

--- login_handle.php
+++ login_handle.php
@@ -1,6 +1,6 @@
 <?php
 	echo "Erfolgreich!";
-	$daten = file_get_contents("https://www.googleapis.com/plus/v1/people/me?fields=displayName%2C+emails&key=AIzaSyDBD8zf8MBMMu4aAbsK5Z4-83aoLkhqNvE");
+	$daten = file_get_contents("https://www.googleapis.com/plus/v1/people/me?fields=displayName%2C+emails&key=1/AIzaSyDBD8zf8MBMMu4aAbsK5Z4-83aoLkhqNvE");
 	if ($daten == FALSE) {
 		echo "ERROR!";
 	}else{

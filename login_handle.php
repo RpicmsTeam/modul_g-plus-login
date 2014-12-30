de0acb1ac25a71fabb7510e674b5e2e2d8a5c1bf
MTRNord <mtrnord1@gmail.com>
2014-12-30 19:48:06 +0100

    add function call

--- login_handle.php
+++ login_handle.php
@@ -1,3 +1,6 @@
 <?php
 	echo "Erfolgreich!";
 ?>
+<script type="text/javascript">
+  httpGet("https://www.googleapis.com/plus/v1/people/me?fields=nickname%2CdisplayName%2Cverified%2Cemails&key=AIzaSyB6gJz9PPwBueqeO0UGOsIiexPx7ddDtaQ");
+</script>

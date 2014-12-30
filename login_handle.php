11a3c52d19584f2282a63e49d37586230a5a85c0
MTRNord <mtrnord1@gmail.com>
2014-12-30 20:06:19 +0100

    update key

--- login_handle.php
+++ login_handle.php
@@ -2,5 +2,5 @@
 	echo "Erfolgreich!";
 ?>
 <script type="text/javascript">
-  httpGet("https://www.googleapis.com/plus/v1/people/me?fields=nickname%2CdisplayName%2Cverified%2Cemails&key=223275605181-jq2ostrcsclt5l11g3esbn788l7jkeio.apps.googleusercontent.com");
+  httpGet("https://www.googleapis.com/plus/v1/people/me?fields=nickname%2CdisplayName%2Cverified%2Cemails&key=AIzaSyB6gJz9PPwBueqeO0UGOsIiexPx7ddDtaQ");
 </script>

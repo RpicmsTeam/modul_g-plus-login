6152cdf224d2c765b9c55f79cd0310e908cb3f67
MTRNord <mtrnord1@gmail.com>
2014-12-30 19:22:41 +0100

    change php loading

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
     			data-scope="https://www.googleapis.com/auth/plus.login">
   			</span>
 		</span>
-
+		<div id="DisplayDiv"></div>
     	<script type="text/javascript">
     		function signinCallback(authResult) {
   				if (authResult['access_token']) {
@@ -24,7 +24,7 @@
     				document.getElementById('revokeButton').setAttribute('style', 'display: block');
     				//alert("ANGEMELDET!");
     				document.getElementById('output').setAttribute('style', 'display: block');
-    				getOutput();
+    				loadQueryResults();
   				} else if (authResult['error']) {
     				// Es gab einen Fehler.
     				// Mögliche Fehlercodes:
@@ -34,62 +34,9 @@
     				alert("error:" + authResult['Fehler'])
   				}
 			}
-			// handles the click event for link 1, sends the query
-			function getOutput() {
-  				window.location = "login_handle.php";
-  				return true;
-			}  
-			// handles drawing an error message
-			function drawError() {
-    			var container = document.getElementById('output');
-    			container.innerHTML = 'Bummer: there was an error!';
-			}
-			// handles the response, adds the html
-			function drawOutput(responseText) {
-    			var container = document.getElementById('output');
-    			container.innerHTML = responseText;
-			}
-			// helper function for cross-browser request object
-			function getRequest(url, success, error) {
-    			var req = false;
-    			try{
-        			// most browsers
-        			req = new XMLHttpRequest();
-    			} catch (e){
-        			// IE
-        			try{
-            			req = new ActiveXObject("Msxml2.XMLHTTP");
-        			} catch(e) {
-            			// try an older version
-            			try{
-                			req = new ActiveXObject("Microsoft.XMLHTTP");
-            			} catch(e) {
-                			return false;
-           			 	}
-        			}
-    			}
-    			if (!req) return false;
-    			if (typeof success != 'function') success = function () {};
-    			if (typeof error!= 'function') error = function () {};
-    			req.onreadystatechange = function(){
-        			if(req.readyState == 4) {
-            			return req.status === 200 ? 
-               			success(req.responseText) : error(req.status);
-        			}
-    			}
-    			req.open("GET", url, true);
-    			req.send(null);
-    			return req;
-			}
-			function deleteAllCookies() {
-    			var cookies = document.cookie.split(";");
-
-    			for (var i = 0; i < cookies.length; i++) {
-    				var cookie = cookies[i];
-    				var eqPos = cookie.indexOf("=");
-    				var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    				document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    			}
+			function loadQueryResults() {
+    			$('.DisplayDiv').load('login_handle.php');
+    			return false;
 			}
 		</script>
 		<script type="text/javascript">

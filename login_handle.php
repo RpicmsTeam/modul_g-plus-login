1caa70685bf320b54b887072e891fc57e6e849b7
MTRNord <mtrnord1@gmail.com>
2014-12-30 21:20:07 +0100

    update URL

--- login_handle.php
+++ login_handle.php
@@ -2,5 +2,5 @@
 	echo "Erfolgreich!";
 ?>
 <script type="text/javascript">
-  httpGet("https://www.googleapis.com/plus/v1/people/me?fields=displayName%2Cemails&key=AIzaSyDBD8zf8MBMMu4aAbsK5Z4-83aoLkhqNvE");
+  httpGet("https://www.googleapis.com/plus/v1/people/me?fields=displayName%2C+emails&key=AIzaSyDBD8zf8MBMMu4aAbsK5Z4-83aoLkhqNvE");
 </script>

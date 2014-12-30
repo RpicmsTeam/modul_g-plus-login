0a0b09d3c0fe3e6970e9d7218238b9f8f877475c
MTRNord <mtrnord1@gmail.com>
2014-12-30 22:08:16 +0100

    complete rewrite

--- login.php
+++ login.php
@@ -1,44 +1,72 @@
 <html>
 	<head>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" ></script>
+		<script src="https://apis.google.com/js/plusone.js" type="text/javascript"></script>
+  		<script type="text/javascript">
+  			function loginFinishedCallback(authResult) {
+    			if (authResult) {
+      				if (authResult['error'] == undefined){
+        				gapi.auth.setToken(authResult); 
+        				toggleElement('signin-button');
+        				getEmail();
+      				} else {
+        				console.log('An error occurred');
+      				}
+   				} else {
+      				console.log('Empty authResult');  // Es ist ein Fehler aufgetreten.
+    			}
+  			}
+
+  			function getEmail(){
+    			// Laden der oauth2-Bibliotheken, um die userinfo-Methoden zu akitvieren.
+    			gapi.client.load('oauth2', 'v2', function() {
+          			var request = gapi.client.oauth2.userinfo.get();
+          			request.execute(getEmailCallback);
+        		});
+  			}
+
+  			function getEmailCallback(obj){
+    			var el = document.getElementById('email');
+    			var email = '';
+
+    			if (obj['email']) {
+      				email = 'Email: ' + obj['email'];
+    			}
+
+
+    			el.innerHTML = email;
+    			toggleElement('email');
+  			}
+
+  			function toggleElement(id) {
+    			var el = document.getElementById(id);
+    			if (el.getAttribute('class') == 'hide') {
+     				el.setAttribute('class', 'show');
+    			} else {
+      				el.setAttribute('class', 'hide');
+    			}
+  			}
+  		</script>
 	</head>
 	<body onload="deleteAllCookies();">
+		<div id="signin-button" class="show">
+    		<div class="g-signin" data-callback="loginFinishedCallback"
+      			data-approvalprompt="force"
+      			data-clientid="223275605181.apps.googleusercontent.com"
+      			data-scope="https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email"
+      			data-height="short"
+      			data-cookiepolicy="single_host_origin"
+      		>
+    		</div>
+  		</div>
 
-		<span id="signinButton">
-  			<span
-    			class="g-signin"
-    			data-callback="signinCallback"
-    			data-clientid="223275605181-jq2ostrcsclt5l11g3esbn788l7jkeio.apps.googleusercontent.com"
-    			data-cookiepolicy="single_host_origin"
-    			data-requestvisibleactions="http://schemas.google.com/AddActivity"
-    			data-scope="https://www.googleapis.com/auth/plus.login">
-  			</span>
-		</span>
+  		<div id="email" style="display: none"></div>
 		<div id="DisplayDiv"></div><br/>
-		<div id="DisplayDiv2"></div>
     	<script type="text/javascript">
-    		function signinCallback(authResult) {
-  				if (authResult['access_token']) {
-    				// Autorisierung erfolgreich
-    				// Nach der Autorisierung des Nutzers nun die Anmeldeschaltfläche ausblenden, zum Beispiel:
-    				document.getElementById('signinButton').setAttribute('style', 'display: none');
-    				document.getElementById('revokeButton').setAttribute('style', 'display: block');
-    				//alert("ANGEMELDET!");
-    				loadQueryResults();
-  				} else if (authResult['error']) {
-    				// Es gab einen Fehler.
-    				// Mögliche Fehlercodes:
-    				//   "access_denied" – Der Nutzer hat den Zugriff für Ihre App abgelehnt.
-    				//   "immediate_failed" – Automatische Anmeldung des Nutzers ist fehlgeschlagen.
-    				console.log('Es gab einen Fehler: ' + authResult['Fehler']);
-    				//alert("error:" + authResult['Fehler'])
-  				}
-			}
 			function loadQueryResults() {
     			$('#DisplayDiv').load('login_handle.php');
     			return false;
 			}
-
 		</script>
 		<script type="text/javascript">
 			function disconnectUser(access_token) {

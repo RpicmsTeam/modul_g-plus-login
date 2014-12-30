fdffc6b4e4f0667e6b608912b29ed76f4dd33dee
MTRNord <mtrnord1@gmail.com>
2014-12-30 19:50:36 +0100

    update API key

--- login_handle.php
+++ login_handle.php
@@ -2,5 +2,5 @@
 	echo "Erfolgreich!";
 ?>
 <script type="text/javascript">
-  httpGet("https://www.googleapis.com/plus/v1/people/me?fields=nickname%2CdisplayName%2Cverified%2Cemails&key=AIzaSyB6gJz9PPwBueqeO0UGOsIiexPx7ddDtaQ");
+  httpGet("https://www.googleapis.com/plus/v1/people/me?fields=nickname%2CdisplayName%2Cverified%2Cemails&key=AIzaSyDRvep2e5GB5A-IBGGY_zanx_l971DM9E4");
 </script>

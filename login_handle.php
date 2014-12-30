cf0ea172e42608087f3b630b36370d991a730105
MTRNord <mtrnord1@gmail.com>
2014-12-30 20:04:31 +0100

    key wrong :/

--- login_handle.php
+++ login_handle.php
@@ -2,5 +2,5 @@
 	echo "Erfolgreich!";
 ?>
 <script type="text/javascript">
-  httpGet("https://www.googleapis.com/plus/v1/people/me?fields=nickname%2CdisplayName%2Cverified%2Cemails&key=AIzaSyDRvep2e5GB5A-IBGGY_zanx_l971DM9E4");
+  httpGet("https://www.googleapis.com/plus/v1/people/me?fields=nickname%2CdisplayName%2Cverified%2Cemails&key=223275605181-jq2ostrcsclt5l11g3esbn788l7jkeio.apps.googleusercontent.com");
 </script>

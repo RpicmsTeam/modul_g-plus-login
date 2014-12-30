4ca6d555cf4ff9b5a7f7d2df6647fbc19d1b7377
MTRNord <mtrnord1@gmail.com>
2014-12-30 18:20:29 +0100

    googles base isn't compatible with git :D

---
+++ login.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+	</head>
+	<body>
+
+		<span id="signinButton">
+  			<span
+    			class="g-signin"
+    			data-callback="signinCallback"
+    			data-clientid="223275605181-hs7d2mmcletb79eksid3v93iilt2vdsv.apps.googleusercontent.com"
+    			data-cookiepolicy="single_host_origin"
+    			data-requestvisibleactions="http://schemas.google.com/AddActivity"
+    			data-scope="https://www.googleapis.com/auth/plus.login">
+  			</span>
+		</span>
+
+		<script type="text/javascript">
+      		(function() {
+       			var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+       			po.src = 'https://apis.google.com/js/client:plusone.js';
+       			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+     		})();
+    	</script>
+    </body>
+</html>

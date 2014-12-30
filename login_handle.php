cab43afd66409c602452aa98d55df6a1bce51059
MTRNord <mtrnord1@gmail.com>
2014-12-30 21:53:29 +0100

    update URL

--- login_handle.php
+++ login_handle.php
@@ -1,6 +1,6 @@
 <?php
 	echo "Erfolgreich!";
-	$daten = file_get_contents("https://www.googleapis.com/plus/v1/people/me?fields=displayName%2C+emails&access_token=1/fFBGRNJru1FQd44AzqT3Zg");
+	$daten = file_get_contents("https://www.googleapis.com/plus/v1/people/me?fields=displayName%2C+emails&key=fFBGRNJru1FQd44AzqT3Zg");
 	if ($daten == FALSE) {
 		echo "ERROR!";
 	}else{

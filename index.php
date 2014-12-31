0f92a27a5cde5d3143874806c188552580d2ed76
MTRNord <mtrnord1@gmail.com>
2014-12-31 20:02:42 +0100

    update index.php

--- index.php
+++ index.php
@@ -16,6 +16,39 @@ if ($root_3[1] == 'core') {
 
 include($root.'/core/libs/OAuth2/vendor/autoload.php')
 
+// Google
+$googleClientConfig = new GoogleClientConfig(
+    json_decode(file_get_contents("client_secrets.json"), true)
+);
+$api = new Api("foo", $googleClientConfig, new SessionStorage(), new \Guzzle\Http\Client());
+
+$context = new Context("mtrnord1@gmail.com", array("read"));
+
+if (false === $accessToken) {
+    /* no valid access token available, go to authorization server */
+    header("HTTP/1.1 302 Found");
+    header("Location: " . $api->getAuthorizeUri($context));
+    exit;
+}
+
+
+try {
+    $cb = new Callback("foo", $clientConfig, new SessionStorage(), new \Guzzle\Http\Client());
+    $cb->handleCallback($_GET);
+
+    header("HTTP/1.1 302 Found");
+    //header("Location: http://www.example.org/index.php");
+} catch (AuthorizeException $e) {
+    // this exception is thrown by Callback when the OAuth server returns a 
+    // specific error message for the client, e.g.: the user did not authorize 
+    // the request
+    echo sprintf("ERROR: %s, DESCRIPTION: %s", $e->getMessage(), $e->getDescription());
+} catch (Exception $e) {
+    // other error, these should never occur in the normal flow
+    echo sprintf("ERROR: %s", $e->getMessage());
+}
+
+$tokenStorage = new SessionStorage();
 
 ?>
 

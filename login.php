f1697d8e116154b44886b17926cae7b4b780d70f
MTRNord <mtrnord1@gmail.com>
2014-12-31 21:48:20 +0100

    try to "reset" login.php

--- login.php
+++ login.php
@@ -17,29 +17,24 @@ if ($root_3[1] == 'core') {
 include($root.'/core/libs/OAuth2/vendor/autoload.php');
 
 use fkooman\OAuth\Client\GoogleClientConfig;
+use fkooman\OAuth\Client\SessionStorage;
 use fkooman\OAuth\Client\Api;
 use fkooman\OAuth\Client\Context;
-use fkooman\Guzzle\Plugin\BearerAuth\BearerAuth;;
+use fkooman\Guzzle\Plugin\BearerAuth\BearerAuth;
 use fkooman\Guzzle\Plugin\BearerAuth\Exception\BearerErrorResponseException;
 use fkooman\OAuth\Client\Callback;
-use fkooman\OAuth\Client\SessionStorage;
-echo "Test1";
 
 // Google
 $googleClientConfig = new GoogleClientConfig(
     json_decode(file_get_contents($root.'/core/backend/admin/modules/modul_g-plus-login/client_secrets.json'), true)
 );
 $api = new Api("foo", $googleClientConfig, new SessionStorage(), new \Guzzle\Http\Client());
-echo "Test2";
 
 $context = new Context("mtrnord1@gmail.com", array("https://www.googleapis.com/auth/plus.login"));
-echo "Test3";
 
 $accessToken = $api->getAccessToken($context);
 if (false === $accessToken) {
     /* no valid access token available, go to authorization server */
-    echo "Test4";
-
     header("HTTP/1.1 302 Found");
     header("Location: " . $api->getAuthorizeUri($context));
     exit;

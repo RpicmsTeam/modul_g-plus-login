2ca6814e8f76fdfe00494c58ef03be916ede4224
MTRNord <mtrnord1@gmail.com>
2014-12-31 21:45:24 +0100

    google sucks :/

--- login.php
+++ login.php
@@ -23,19 +23,23 @@ use fkooman\Guzzle\Plugin\BearerAuth\BearerAuth;;
 use fkooman\Guzzle\Plugin\BearerAuth\Exception\BearerErrorResponseException;
 use fkooman\OAuth\Client\Callback;
 use fkooman\OAuth\Client\SessionStorage;
-
+echo "Test1";
 
 // Google
 $googleClientConfig = new GoogleClientConfig(
     json_decode(file_get_contents($root.'/core/backend/admin/modules/modul_g-plus-login/client_secrets.json'), true)
 );
 $api = new Api("foo", $googleClientConfig, new SessionStorage(), new \Guzzle\Http\Client());
+echo "Test2";
 
 $context = new Context("mtrnord1@gmail.com", array("https://www.googleapis.com/auth/plus.login"));
+echo "Test3";
 
 $accessToken = $api->getAccessToken($context);
 if (false === $accessToken) {
     /* no valid access token available, go to authorization server */
+    echo "Test4";
+
     header("HTTP/1.1 302 Found");
     header("Location: " . $api->getAuthorizeUri($context));
     exit;

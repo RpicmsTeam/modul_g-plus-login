1c0c3e27cd2453d20b03efa27a57eeaabecc2d0a
MTRNord <mtrnord1@gmail.com>
2014-12-31 21:31:21 +0100

    update Lib integration

--- handle.php
+++ handle.php
@@ -1,4 +1,27 @@
 <?php
+
+###############################
+# include files from root dir #
+###############################
+$root_1 = realpath($_SERVER["DOCUMENT_ROOT"]);
+$currentdir = getcwd();
+$root_2 = str_replace($root_1, '', $currentdir);
+$root_3 = explode("/", $root_2);
+if ($root_3[1] == 'core') {
+  echo $root_3[1];
+  $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+}else{
+  $root = $root_1 . '/' . $root_3[1];
+}
+
+include($root.'/core/libs/OAuth2/vendor/autoload.php');
+
+
+use fkooman\Guzzle\Plugin\BearerAuth\Exception\BearerErrorResponseException;
+use fkooman\OAuth\Client\Callback;
+use fkooman\OAuth\Client\SessionStorage;
+
+
 try {
     $cb = new Callback("foo", $clientConfig, new SessionStorage(), new \Guzzle\Http\Client());
     $cb->handleCallback($_GET);

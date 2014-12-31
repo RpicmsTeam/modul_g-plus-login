6d61324ee17d13e0376ad61420c6494b01104f92
MTRNord <mtrnord1@gmail.com>
2014-12-31 22:13:13 +0100

    fix variable missmatch

--- handle.php
+++ handle.php
@@ -27,18 +27,20 @@ $googleClientConfig = new GoogleClientConfig(
 );
 
 try {
-    $cb = new Callback("foo", $googleClientConfig, new SessionStorage(), new \Guzzle\Http\Client());
+    $tokenStorage = new fkooman\OAuth\Client\SessionStorage();
+    $httpClient = new Guzzle\Http\Client();
+    $cb = new fkooman\OAuth\Client\Callback("foo", $GoogleClientConfig, $tokenStorage, $httpClient);
     $cb->handleCallback($_GET);
-
     header("HTTP/1.1 302 Found");
     header("Location: http://".$_SERVER['SERVER_NAME'].$root."/index.php");
-} catch (AuthorizeException $e) {
-    // this exception is thrown by Callback when the OAuth server returns a 
-    // specific error message for the client, e.g.: the user did not authorize 
+    exit;
+} catch (fkooman\OAuth\Client\Exception\AuthorizeException $e) {
+    // this exception is thrown by Callback when the OAuth server returns a
+    // specific error message for the client, e.g.: the user did not authorize
     // the request
-    echo sprintf("ERROR: %s, DESCRIPTION: %s", $e->getMessage(), $e->getDescription());
+    die(sprintf("ERROR: %s, DESCRIPTION: %s", $e->getMessage(), $e->getDescription()));
 } catch (Exception $e) {
     // other error, these should never occur in the normal flow
-    echo sprintf("ERROR: %s", $e->getMessage());
+    die(sprintf("ERROR: %s", $e->getMessage()));
 }
 ?>

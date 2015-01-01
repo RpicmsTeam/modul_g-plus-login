d87a16f0526336ec8836d1287fc070ce8cd460d2
MTRNord <mtrnord1@gmail.com>
2015-01-01 12:51:45 +0100

    try to change color

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 if(!isset($_COOKIE['PHPSESSID'])) {
 	echo "<button onClick='self.location='../../core/backend/admin/modules/modul_g-plus-login/login.php''>Login with Google</button>";
 }else{
-	echo "<b>Logged in!</b>";
+	echo "<b style='textcolor: white'>Logged in!</b>";
 }
 
 

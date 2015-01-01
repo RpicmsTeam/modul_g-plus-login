e9d13d633c5cfea84587dac115a4d04f489bfd17
MTRNord <mtrnord1@gmail.com>
2015-01-01 13:20:14 +0100

    renew if

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-if(!isset($_COOKIE['PHPSESSID'])) {
+if(!isset($_COOKIE['PHPSESSID']) || (session_get_cookie_params("domain") == "rpi.nordgedanken.de") {
 	echo "<button onClick='self.location='../../core/backend/admin/modules/modul_g-plus-login/login.php''>Login with Google</button>";
 }else{
 	echo "<b style='color: white'>Logged in!</b>";

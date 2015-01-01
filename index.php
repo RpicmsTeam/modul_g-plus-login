db44f66bb5e5816b5f8e91a50102c03a9f55847f
MTRNord <mtrnord1@gmail.com>
2015-01-01 13:28:51 +0100

    set right name

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if(!isset($_COOKIE['PHPSESSID'])) {
 	echo "<button onClick='self.location='../../core/backend/admin/modules/modul_g-plus-login/login.php''>Login with Google</button>";
 }else{
 	echo "<b style='color: white'>Logged in!</b>";
-	echo "Value is: " . $_COOKIE[$cookie_name];
+	echo "Value is: " . $_COOKIE['PHPSESSID'];
 }
 
 

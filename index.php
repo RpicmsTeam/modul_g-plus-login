57f7a4f5e8d037182fe7a3ba50e3f4a2cc7039c5
MTRNord <mtrnord1@gmail.com>
2015-01-01 13:34:36 +0100

    I missed some thing :D

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $context = new Context("mtrnord1@gmail.com", array("https://www.googleapis.com/a
 $accessToken = $api->getAccessToken($context);
 
 
-if(!isset($_COOKIE['PHPSESSID']) || $_COOKIE['PHPSESSID'] == $accessToken) {
+if(!isset($_COOKIE['PHPSESSID']) || !$_COOKIE['PHPSESSID'] == $accessToken) {
 	echo "<button onClick='self.location='../../core/backend/admin/modules/modul_g-plus-login/login.php''>Login with Google</button>";
 }else{
 	echo "<b style='color: white'>Logged in!</b>";

87e466b58eb871f2dca1683074b59faa43b688db
MTRNord <mtrnord1@gmail.com>
2015-01-01 12:45:02 +0100

    more things :D

--- login.php
+++ login.php
@@ -44,6 +44,28 @@ if (false === $accessToken) {
     header("Location: http://rpi.nordgedanken.de/cms_new/index.php");
 }
 
+try {
+    $client = new Guzzle\Http\Client();
+    $bearerAuth = new fkooman\Guzzle\Plugin\BearerAuth\BearerAuth($accessToken->getAccessToken());
+    $client->addSubscriber($bearerAuth);
+    $response = $client->get($apiUri)->send();
+    header("Content-Type: application/json");
+    echo $response->getBody();
+} catch (fkooman\Guzzle\Plugin\BearerAuth\Exception\BearerErrorResponseException $e) {
+    if ("invalid_token" === $e->getBearerReason()) {
+        // the token we used was invalid, possibly revoked, we throw it away
+        $api->deleteAccessToken($context);
+        $api->deleteRefreshToken($context);
+        /* no valid access token available, go to authorization server */
+        header("HTTP/1.1 302 Found");
+        header("Location: ".$api->getAuthorizeUri($context));
+        exit;
+    }
+    throw $e;
+} catch (Exception $e) {
+    die(sprintf('ERROR: %s', $e->getMessage()));
+}
+
 
 
 

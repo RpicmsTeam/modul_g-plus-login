5c82d242854af0f98e2ab5a91ab407d8ebb77366
MTRNord <mtrnord1@gmail.com>
2015-01-01 13:27:47 +0100

    I thing it isn't showed :/

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
-if(!isset($_COOKIE['PHPSESSID']) || session_get_cookie_params("domain")) {
+if(!isset($_COOKIE['PHPSESSID'])) {
 	echo "<button onClick='self.location='../../core/backend/admin/modules/modul_g-plus-login/login.php''>Login with Google</button>";
 }else{
 	echo "<b style='color: white'>Logged in!</b>";
+	echo "Value is: " . $_COOKIE[$cookie_name];
 }
 
 

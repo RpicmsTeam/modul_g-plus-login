75ab704344092ab891ace0834d4c2b8785e02b1f
MTRNord <mtrnord1@gmail.com>
2015-01-02 17:02:49 +0100

    fix css

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $accessToken = $api->getAccessToken($context);
 
 
 if(!isset($_COOKIE['PHPSESSID']) || !$_COOKIE['PHPSESSID'] == $accessToken) {
-	echo "<a onClick=\"self.location='../../core/backend/admin/modules/modul_g-plus-login/login.php'\" class=\"btn btn-block btn-social btn-google-plus\"><i class=\"fa fa-twitter\"></i>Login with Google</a>";
+	echo "<a onClick=\"self.location='../../core/backend/admin/modules/modul_g-plus-login/login.php'\" class=\"btn btn-block btn-social btn-google-plus\"><i class=\"fa fa-google-plus\"></i>Login with Google</a>";
 }else{
 	echo "<b style='color: white'>Logged in!</b>";
 	echo "Value is: " . $_COOKIE['PHPSESSID'];

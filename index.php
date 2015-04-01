eab78ac12d9f08aa984ce80e5bff9e0f5512e3da
MTRNord <mtrnord1@gmail.com>
2015-04-01 19:24:49 +0200

    try to fix cookie problem

--- index.php
+++ index.php
@@ -11,15 +11,14 @@ $googleClientConfig = new GoogleClientConfig(
 );
 $api = new Api("foo", $googleClientConfig, new SessionStorage(), new \Guzzle\Http\Client());
 $context = new Context("mtrnord1@gmail.com", array("https://www.googleapis.com/auth/plus.login"));
-$accessToken = $api->getAccessToken($context);
+#$accessToken = $api->getAccessToken($context);
 
 
-if(!isset($_COOKIE['PHPSESSID']) || !$_COOKIE['PHPSESSID'] == $accessToken) {
-	echo "<a onClick=\"self.location='../../core/backend/admin/modules/modul_g-plus-login/login.php'\" class=\"btn btn-block btn-social btn-google-plus\"><i class=\"fa fa-google-plus\"></i>Login with Google+</a>";
-}else{
-	echo "<b style=\"color: white; text-align:center;\" class=\"btn btn-block btn-social btn-google-plus\"><i class=\"fa fa-google-plus\"></i>Logged in!</b>";
-}
+#if(!isset($_COOKIE['PHPSESSID']) || !$_COOKIE['PHPSESSID'] == $accessToken) {
+#	echo "<a onClick=\"self.location='../../core/backend/admin/modules/modul_g-plus-login/login.php'\" class=\"btn btn-block btn-social btn-google-plus\"><i class=\"fa fa-google-plus\"></i>Login with Google+</a>";
+#}else{
+#	echo "<b style=\"color: white; text-align:center;\" class=\"btn btn-block btn-social btn-google-plus\"><i class=\"fa fa-google-plus\"></i>Logged in!</b>";
+#}
 
 
 ?>
-
